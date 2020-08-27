a399536686274ccc7a76873d64a1d8ef3e155990
Marcus Schuff <marcus.schuff@my.ndsu.edu>
2020-08-27 13:14:21 -0500

    Add project files.

---
+++ DateAndTimePicker/DTPicker.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace DateAndTimePicker
+{
+    partial class DTPicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblCustomMessage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TimePicker = new System.Windows.Forms.DateTimePicker();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.lblCustomMessage);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.datePicker);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.TimePicker);
+            this.flowLayoutPanel1.Controls.Add(this.btnOK);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(16, 16);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(16, 16, 16, 16);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(279, 248);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // lblCustomMessage
+            // 
+            this.lblCustomMessage.AutoSize = true;
+            this.lblCustomMessage.Location = new System.Drawing.Point(10, 6);
+            this.lblCustomMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCustomMessage.Name = "lblCustomMessage";
+            this.lblCustomMessage.Size = new System.Drawing.Size(230, 21);
+            this.lblCustomMessage.TabIndex = 5;
+            this.lblCustomMessage.Text = "Please Pick a date and time:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 10, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Date:";
+            // 
+            // datePicker
+            // 
+            this.datePicker.CalendarFont = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datePicker.Location = new System.Drawing.Point(11, 63);
+            this.datePicker.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(222, 27);
+            this.datePicker.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 105);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 10, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Time:";
+            // 
+            // TimePicker
+            // 
+            this.TimePicker.CalendarFont = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimePicker.CustomFormat = "HH:mm";
+            this.TimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.TimePicker.Location = new System.Drawing.Point(11, 131);
+            this.TimePicker.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.TimePicker.Name = "TimePicker";
+            this.TimePicker.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.TimePicker.ShowUpDown = true;
+            this.TimePicker.Size = new System.Drawing.Size(222, 27);
+            this.TimePicker.TabIndex = 2;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(10, 167);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(105, 38);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // DTPicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 280);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "DTPicker";
+            this.Padding = new System.Windows.Forms.Padding(16, 16, 16, 16);
+            this.Text = "Date and Time Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker TimePicker;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblCustomMessage;
+    }
+}
+
---
+++ DateAndTimePicker/DTPicker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateAndTimePicker
+{
+    public partial class DTPicker : Form
+    {
+
+        public string SelectedDate { get; set; } 
+        public string SelectedTime { get; set; }
+
+        public DTPicker(string greeting = "Please pick a date and time.")
+        {
+            InitializeComponent();
+
+            lblCustomMessage.Text = greeting;
+
+            datePicker.Value = DateTime.Today;
+            TimePicker.Value = DateTime.Now;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            SelectedDate = datePicker.Value.ToString("MM-dd-yyyy");
+            SelectedTime = TimePicker.Value.ToString("HH:mm");
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+
+            this.SelectedDate = datePicker.Value.ToString("MM-dd-yyyy");
+            this.SelectedTime = TimePicker.Value.ToString("HH:mm");
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+
+        }
+
+
+    }
+}
